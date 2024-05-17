dd6635089d27983cc1b78f905bbe812227472bcc
bean <titless.skittle@gmail.com>
2024-05-17 17:32:01 +0100

    Implemented Flight's view of Pilots and StaffWorkers

--- Spoonbill.Wpf/Frontend/Viewmodels/Crud/IntrospectViewModels/References/PilotReference.cs
+++ Spoonbill.Wpf/Frontend/Viewmodels/Crud/IntrospectViewModels/References/PilotReference.cs
@@ -6,10 +6,30 @@ namespace Spoonbill.Wpf.Frontend.Viewmodels.Crud.IntrospectViewModels.References
 [UsedImplicitly(ImplicitUseKindFlags.Access, ImplicitUseTargetFlags.Members)]
 public class PilotReference
 {
+    private bool Equals(PilotReference other)
+    {
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((PilotReference)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+
     public int Id { get; }
     public string FullName { get; }
 
     public string TypeRating { get; }
+
+    public string ViewText => $"{Id} | {FullName} | {TypeRating}";
 
     public PilotReference(Pilot pilot)
     {
--- Spoonbill.Wpf/Frontend/Viewmodels/Crud/IntrospectViewModels/References/StaffWorkerReference.cs
+++ Spoonbill.Wpf/Frontend/Viewmodels/Crud/IntrospectViewModels/References/StaffWorkerReference.cs
@@ -6,9 +6,29 @@ namespace Spoonbill.Wpf.Frontend.Viewmodels.Crud.IntrospectViewModels.References
 [UsedImplicitly(ImplicitUseKindFlags.Access, ImplicitUseTargetFlags.Members)]
 public class StaffWorkerReference
 {
+    private bool Equals(StaffWorkerReference other)
+    {
+        return Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((StaffWorkerReference)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+
     public int Id { get; }
     public string Role { get; }
     public string FullName { get; }
+
+    public string ViewText => $"{Id} | {FullName} | {Role}";
 
     public StaffWorkerReference(StaffWorker staffWorker)
     {
